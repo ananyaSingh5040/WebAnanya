c5abe4932c1a5898af7b6510b78cda355967e0fe
Ananya Singh <singhananya5040@gmail.com>
2025-02-06 10:23:15 +0530

    06-02-25

--- JavaScript/async-await.js
+++ JavaScript/async-await.js
@@ -6,7 +6,7 @@
 // let b= promises();
 // console.log(b); async fucnction returns promises.
 async function getData(){
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1'); //await se synchronously behave karega that is jab tak data nai aa jata tab tak ruka rahega.
     let data= await response.json(); //parsing of data.
     console.log(data);
 }
